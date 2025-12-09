14fbc12417015e79e25baf8e5c0ad02fdf1f97a0
Shubham <shubhamgogawale1@gmail.com>
2025-12-09 18:00:54 +0530

    fix: update Node.js version to 22.x in package.json and vercel.json; add desktop.ini

--- config/db.js
+++ config/db.js
@@ -1,16 +1,3 @@
-// config/db.js
-// import mongoose from "mongoose";
-
-// export const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI);
-//     console.log("MongoDB connected");
-//   } catch (err) {
-//     console.error("MongoDB connection error", err.message);
-//     process.exit(1);
-//   }
-// };
-
 import mongoose from "mongoose";
 
 let isConnected = false;
